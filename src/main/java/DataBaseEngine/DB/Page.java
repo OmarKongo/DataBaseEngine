73e74ed751eda9361187b0628770f86feb435ffa
YousefElbrolosy <yousefelbrolosy@gmail.com>
2024-03-12 07:24:23 +0200

    added toString of Page and Tuple, as well as imported rowcount from DBApp Config to be used in page class

--- src/main/java/DataBaseEngine/DB/Page.java
+++ src/main/java/DataBaseEngine/DB/Page.java
@@ -1,21 +1,67 @@
 package DataBaseEngine.DB;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.Properties;
 import java.util.Vector;
 
 public class Page implements Serializable{
+    // public because it should be accessed by Table (i think) and static because
+    // the variable belongs to the class itself rather than to any specific instance of the class
+    public static int N;
+    Vector<Tuple> tuplesInPage = new Vector<Tuple>();
     //Vector<Tuple> tuplesInPage = new Vector<Tuple>();
-    Vector<Object> tuplesInPage = new Vector<Object>();
+    
 
-    public Page(Vector<Object> tuplesInPage) {
+    public Page(Vector<Tuple> tuplesInPage) {
+
         this.tuplesInPage = tuplesInPage;
+        try{
+            Properties properties = new Properties();
+            properties.load(new FileInputStream("DBApp.config"));
+            Page.N = Integer.parseInt(properties.getProperty("MaximumRowsCountinPage"));
+        }
+        catch(FileNotFoundException e){
+            System.out.print(e.getStackTrace());
+        } 
+        catch(IOException e){
+            System.out.print(e.getStackTrace());
+        } 
+       
+       
+    }
+
+
+
+    public static int initDBAppConfig() {
+        Properties properties = new Properties();
+        int n = 0;
+        try{
+            
+            properties.load(new FileInputStream("DBApp.config"));
+            n = Integer.parseInt(properties.getProperty("MaximumRowsCountinPage"));
+        }
+        catch(FileNotFoundException e){
+            System.out.print(e.getStackTrace());
+        } 
+        catch(IOException e){
+            System.out.print(e.getStackTrace());
+        } 
+        return n;
     }
 
 
     public String toString(){
-        String logic = "do logic here";
-
-        return logic;
+        String res = "";
+        for(int i = 0; i<tuplesInPage.size(); i++){
+            res = res + tuplesInPage.elementAt(i).toString();
+            if(i!=tuplesInPage.size()-1){
+                res = res + ",";
+            }
+        }
+        return res;
     }
 
     public void insertIntoPage(){
---
+++ src/main/java/DataBaseEngine/DB/Tuple.java
@@ -0,0 +1,32 @@
+package DataBaseEngine.DB;
+
+import java.util.Vector;
+
+public class Tuple {
+    Vector<Object> attributesInTuple = new Vector<Object>();
+
+    public Tuple(Vector<Object> attributesInTuple) {
+        this.attributesInTuple = attributesInTuple;
+    }
+
+    public Vector<Object> getAttributesInTuple() {
+        return attributesInTuple;
+    }
+
+    public void setAttributesInTuple(Vector<Object> attributesInTuple) {
+        this.attributesInTuple = attributesInTuple;
+    }
+
+    public String toString(){
+        String res = "";
+        for(int i = 0; i<attributesInTuple.size(); i++){
+            //what does the toString of Object class do?
+            res = res + attributesInTuple.elementAt(i).toString();
+            if(i!=attributesInTuple.size()-1){
+                res = res + ",";
+            }
+        }
+        return res;
+    }
+    
+}
