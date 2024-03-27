e9405a9728c7675ad09a8915b4ec5e057404a611
Paula Hanna <128493887+PaulaMaged@users.noreply.github.com>
2024-03-27 15:39:59 +0200

    merge insertIntoTable src file

--- src/main/java/DataBaseEngine/DB/DBApp.java
+++ src/main/java/DataBaseEngine/DB/DBApp.java
@@ -3,6 +3,7 @@ package DataBaseEngine.DB;
 
 
 import java.util.Iterator;
+import java.util.Properties;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -16,13 +17,9 @@ import com.opencsv.CSVReaderBuilder;
 import com.opencsv.CSVWriter;
 
 public class DBApp {
-	final static String csvPath = "files\\metadata.csv";
-	//contains serialisation names of tables
-	ArrayList<String> tablesFileNames;
+	final static String csvPath = "metadata.csv";
 
-
-	public DBApp( ) throws IOException{
-		this.tablesFileNames = new ArrayList<String>();
+	public DBApp( ){
 		this.init();
 	}
 
@@ -31,7 +28,7 @@ public class DBApp {
 	// execute at application startup 
 
 	
-	public void init( ) throws IOException{
+	public void init( ){
 		CSVWriter writer =null;
 		try {
 			
@@ -40,66 +37,15 @@ public class DBApp {
 						,"Clustering Key","IndexName","IndexType"};
 			
 			writer.writeNext(header);
+			 writer.flush();
+	            writer.close();
 		}
 		catch(Exception ex) {
 			ex.printStackTrace();
-		} finally {
-			writer.flush();
-			writer.close();
 		}
 		
 	}
-
-	public ArrayList<String> getTablesFileNames() {
-		return this.tablesFileNames;
-	}
-
-	public void setTablesFileNames(ArrayList<String> tables) {
-		this.tablesFileNames = tables;
-	}
 	
-	public static void checkData(String tableName,Hashtable<String,Object> data) throws Exception {
-		CSVReader csvReader = new CSVReaderBuilder(new FileReader(csvPath)) 
-                .withSkipLines(1) 
-                .build();
-		String[] nextRecord;boolean flag = false;
-		Enumeration<Object> values = data.elements();
-        Enumeration<String> keys = data.keys();
-        Hashtable<String,String> collector = new Hashtable<String,String>();
-        // this loop iterate over the csv file untill the table is founded
-        // if it's not founded then an exception will be thrown
-		while ((nextRecord = csvReader.readNext()) != null) { 
-            
-            if(nextRecord[0].equals(tableName)) {
-            	flag = true;
-            	break;
-            }
-            else
-            	continue;
-        }
-		if(!flag)
-			throw new Exception("Invalid Table");
-		// this loop insert all the original attributes of the table inside collector hashtable
-		while(nextRecord!=null && nextRecord[0].equals(tableName)) {
-			collector.put(nextRecord[1],nextRecord[2]);
-			nextRecord = csvReader.readNext();
-			
-		}
-		// this loop checks the entered data and throws exception if anything mismatches the original attributes 
-		while(keys.hasMoreElements()) {
-			String key = keys.nextElement();
-			if(collector.containsKey(key)){
-				if(!(values.nextElement().getClass().equals(Class.forName(collector.get(key)))))
-					throw new Exception("Mismatch type");
-				
-			}
-			else
-				throw new Exception("mismatch key");
-			
-    	}
-	}
-
-
 	// following method creates one table only
 	// strClusteringKeyColumn is the name of the column that will be the primary
 	// key and the clustering column as well. The data type of that column will
@@ -113,15 +59,12 @@ public class DBApp {
 							Hashtable<String,String> htblColNameType) throws DBAppException, IOException{
 
 								
-
-								Table t = new Table(strTableName,strClusteringKeyColumn,htblColNameType);
-								//in Kongo he there was htblcolName provided to add Table is this correct?
-								t.addTable(strTableName,strClusteringKeyColumn,DBApp.csvPath);
-								//String serName = t.serialiseTable();
-								String serName = Serialize.serializeTable(t);
-								this.getTablesFileNames().add(serName);
+                 
+								Table T = new Table(strTableName,strClusteringKeyColumn,htblColNameType);
+								T.addTable(strTableName,strClusteringKeyColumn,csvPath);
+								Serialize.Table(T);			
+							
 	}
-
 
 	// following method creates a B+tree index 
 	public void createIndex(String   strTableName,
@@ -136,8 +79,24 @@ public class DBApp {
 	// htblColNameValue must include a value for the primary key
 	public void insertIntoTable(String strTableName, 
 								Hashtable<String,Object>  htblColNameValue) throws DBAppException{
+;
+		
+		try {
+			Table.checkData(strTableName, htblColNameValue, csvPath);
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		Table T = Deserialize.Table(strTableName);
+		Tuple record = new Tuple(T.getStrClusteringKeyColumn(),htblColNameValue.keys(),htblColNameValue.elements());	
+		try {
+			T = T.insertIntoTable(record);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Serialize.Table(T);
 	
-		throw new DBAppException("not implemented yet");
 	}
 
 
@@ -171,9 +130,10 @@ public class DBApp {
 	}
 
 
+	@SuppressWarnings({ "removal" })
 	public static void main( String[] args ){
 	
-	try{
+		try{
 			String strTableName = "Student";
 			DBApp	dbApp = new DBApp( );
 			
@@ -234,7 +194,7 @@ public class DBApp {
 		}
 		catch(Exception exp){
 			exp.printStackTrace( );
-		}
+		}}
 	}
-
-}
+		
+	
--- src/main/java/DataBaseEngine/DB/Table.java
+++ src/main/java/DataBaseEngine/DB/Table.java
@@ -1,7 +1,9 @@
 package DataBaseEngine.DB;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -9,15 +11,23 @@ import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.TreeMap;
+import java.util.Vector;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import com.opencsv.CSVWriter;
+
+
 
 public class Table implements Serializable{
     //Is it correct that only the pageFileName are persistent?
 
-    private ArrayList<String> pageFileNames;
+    
+	private Vector<Page> pages ;
     private String strTableName;
     private String strClusteringKeyColumn;
     //tranisent since already in metadata?
@@ -39,13 +49,16 @@ public class Table implements Serializable{
     }
     public Table(String strTableName, String strClusteringKeyColumn,
             Hashtable<String, String> htblColNameType) {
-        pagesCounter = 0;
-        this.pageFileNames = new ArrayList<>();
+        pagesCounter = 1;
+        pages = new Vector<Page>();
         this.strTableName = strTableName;
         this.strClusteringKeyColumn = strClusteringKeyColumn;
         this.htblColNameType = htblColNameType;
 
     }
+    public Vector<Page> getPages() {
+		return pages;
+	}
 
     public int getPagesCounter() {
         return pagesCounter;
@@ -55,9 +68,7 @@ public class Table implements Serializable{
         this.pagesCounter = pagesCounter;
     }
 
-    public ArrayList<String> getPageFileNames() {
-        return pageFileNames;
-    }
+    
 
     public String getStrTableName() {
         return strTableName;
@@ -79,21 +90,10 @@ public class Table implements Serializable{
         return htblColNameType;
     }
 
-    public void setHtblColNameType(Hashtable<String, String> htblColNameType) {
-        this.htblColNameType = htblColNameType;
-    }
-
-
-
-
-
-
-
-    public void addTable(String tableName,String pK,String filePath) throws IOException {
-        CSVWriter writer = null;
+    public void addTable(String tableName,String pK,String filePath) {
         try {
-            checkDataType(this.htblColNameType);
-            writer = new CSVWriter(new FileWriter(filePath,true));
+            checkDataType(this.htblColNameType,filePath,tableName);
+            CSVWriter writer = new CSVWriter(new FileWriter(filePath,true));
             Enumeration<String> types = this.htblColNameType.elements();
             Enumeration<String> keys = this.htblColNameType.keys();
             String []line =  new String[6];
@@ -102,26 +102,45 @@ public class Table implements Serializable{
                 line = insertLine(line,tableName,keys.nextElement(),types.nextElement(),pK);
                 writer.writeNext(line);
             }
-
+            writer.flush();
+            writer.close();
         }
         catch(Exception ex) {
             ex.printStackTrace();
-        } finally {
-            writer.flush();
-            writer.close();
         }
     
     }
 
-    public static void checkDataType(Hashtable<String,String> htblColNameType) throws DBAppException{
+    public static void checkDataType(Hashtable<String,String> htblColNameType,String filePath,String tableName) throws DBAppException, IOException{
 		for(String s : htblColNameType.values()){
 			if(!(s.equals("java.lang.Integer") || s.equals("java.lang.String") || s.equals("java.lang.Double"))){
 				throw new DBAppException("Column DataType invalid");
 			}
 		}
+		CSVReader csvReader = null;
+		try {
+			csvReader = new CSVReaderBuilder(new FileReader(filePath)) 
+			        .withSkipLines(1) 
+			        .build();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String[] nextRecord;boolean flag = false;
+		
+        // this loop iterate over the csv file untill the table is founded
+        // if it's  founded then an exception will be thrown
+		while ((nextRecord = csvReader.readNext()) != null) { 
+            
+            if(nextRecord[0].equals(tableName))
+            	throw new DBAppException("Duplicate Table");
+            	
+            else
+            	continue;
+        
 
 	}
-
+    }
     public static String[] insertLine(String []line,String tableName,String name, String type,String primaryKey) {
 		
 		line[0] = tableName;line[1] = name; line[2] = type;
@@ -133,49 +152,133 @@ public class Table implements Serializable{
 	
         return line;
     }
-/* 
-    public Page createPage(String tableName) {
-		int counter = this.getPagesCounter();
-		String pageName = tableName+counter;
-		Page p = new Page(pageName);
-		this.setPagesCounter(++counter);
-        
-        String serializedName = Serialize.serializePage(p);
-		this.getPageFileNames().add(serializedName);
-		
-		
-		return p;
-		
-	}
-*/
-    public Page createPage(){
+    
+    public static void checkData(String tableName,Hashtable<String,Object> data,String filePath) throws Exception {
+		CSVReader csvReader = new CSVReaderBuilder(new FileReader(filePath)) 
+                .withSkipLines(1) 
+                .build();
+		String[] nextRecord;boolean flag = false;
+		Enumeration<Object> values = data.elements();
+        Enumeration<String> keys = data.keys();
+        Hashtable<String,String> collector = new Hashtable<String,String>();
+        // this loop iterate over the csv file untill the table is founded
+        // if it's not founded then an exception will be thrown
+		while ((nextRecord = csvReader.readNext()) != null) { 
             
-        int counter = this.getPagesCounter();
+            if(nextRecord[0].equals(tableName)) {
+            	flag = true;
+            	break;
+            }
+            else
+            	continue;
+        }
+		if(!flag)
+			throw new Exception("Invalid Table");
+		// this loop insert all the original attributes of the table inside collector hashtable
+		while(nextRecord!=null && nextRecord[0].equals(tableName)) {
+			collector.put(nextRecord[1],nextRecord[2]);
+			nextRecord = csvReader.readNext();
+			
+		}
+		// this loop checks the entered data and throws exception if anything mismatches the original attributes 
+		while(keys.hasMoreElements()) {
+			String key = keys.nextElement();
+			if(collector.containsKey(key)){
+				if(!(values.nextElement().getClass().equals(Class.forName(collector.get(key)))))
+					throw new Exception("Mismatch type");
+				
+			}
+			else
+				throw new Exception("mismatch key  "+key);
+			
+    	}
+	}
+   
+    public String setNameForpage() {
+    	int counter = this.getPagesCounter();
 		String pageName = this.getStrTableName()+counter;
-		Page p = new Page(pageName);
-        this.setPagesCounter(++counter);
-        return p;
-
+		this.setPagesCounter(++counter);
+		return pageName;
     }
-
-
-    public void appendPage(String pageFileName){
-        pageFileNames.add(pageFileName);
+     
+    public  Table insertIntoTable(Tuple T) throws Exception {
+    	Page p = null;String pageName;boolean flag = false;
+           int maxKeyInPreviousPage;// this attribute connect the pages with each other maxLastpage = minNextPage
+    	//Base Case
+    
+    	if(this.getPages().size()==0) {
+    		 p = new Page(this.setNameForpage());
+    		 p.getPageProp().put(p.getName(),new Pair(0));
+    		 this.getPages().add(p);
+    		 T.addTuple(p);
+    		 p = p.updateMax();
+    		 
+    		 Serialize.Page(p);
+    		 
+    	}
+    	
+    	else {  // Search for the right page to insert in
+    		
+    		int index = this.binarySearch(T);
+    		 pageName = this.getPages().get(index).getName();
+    		 p = Deserialize.Page(pageName);
+    	
+    		if(p.tupleFounded(T))
+    	        	throw new DBAppException("Duplicate Tuple");
+    		p = T.addTuple(p);
+    		if(!p.overFlowed()) {
+    			p = p.updateMax();
+    			Serialize.Page(p);
+    	    	//ADDED SUCCESSFULLY
+    	    }
+    	    else {
+    	    	//OVERFLOW ON PAGE BUT STILL ADDED
+    	    	T = p.getTuplesInPage().remove(p.getMaxCount());
+    	    	p = p.updateMax();
+          maxKeyInPreviousPage = p.getTuplesInPage().lastElement().getPK();
+                   Serialize.Page(p);
+                 //OVERFLOW ON THE LAST PAGE
+                if(index == this.getPages().size()-1) { // if the overflow in the last page, I will create a new page without going through any loops
+                	p = new Page(this.setNameForpage());p.getPageProp().put(p.getName(),new Pair(0)); 
+                	this.getPages().add(p);p = p.updateMin(maxKeyInPreviousPage);	
+                	T.addTuple(p);p = p.updateMax();Serialize.Page(p);
+                }
+                
+                else { // handle the overflow of pages by setting flag = true 
+                	 /// if flag is still false this means that the last page has an overflow 
+                	while(index!=this.getPages().size()-1) {
+                	pageName = this.getPages().get(index+1).getName();
+                	//System.out.println(pageName);
+                	p = Deserialize.Page(pageName);
+                	T.addTuple(p);
+                	if(!p.overFlowed()) {
+                		p = p.updateMin(maxKeyInPreviousPage);p = p.updateMax();
+                		Serialize.Page(p);
+                		flag = true;
+                		break;
+                	}
+                	else {
+                		T = p.getTuplesInPage().remove(p.getMaxCount());
+                		p = p.updateMax();p = p.updateMin(maxKeyInPreviousPage);
+                        maxKeyInPreviousPage = p.getTuplesInPage().lastElement().getPK();
+                        Serialize.Page(p);
+                	}
+                	index++;
+                }
+                	if(!flag) { // this if handles the last overflow can happen
+                		p = new Page(this.setNameForpage());p.getPageProp().put(p.getName(),new Pair(0));
+                		this.getPages().add(p);p = p.updateMin(maxKeyInPreviousPage);	
+                    	T.addTuple(p);p = p.updateMax();Serialize.Page(p);
+                	}
+                }
+    	    	
+    	    }
+    	}
+    	return this;
+    	
     }
     
-   // public void insertRow(){
-        //logic to choose page
-        //deserialise correct page Page.p
-        //pinsertIntoPage()
-
-        //if specific chosen page is full,
-        //create new page
-        //insert in it
-        //serialise
-        //append page file name 
-    //}
-
-    public void updateRow(){
+	public void updateRow(){
         
     }
 
@@ -186,51 +289,45 @@ public class Table implements Serializable{
     public void selectRow(){
 
     }
-
-    /**
-     * @author Brolosy
-     * @param p the table to be serialised
-     * serialises table
-     * @return returns filename of type .ser
-     */
-
-    public String serialiseTable(){
-        String filename = this.getStrTableName()+".ser";
-        try{
-            //this.pageFileNames.add(filename);
-            FileOutputStream f = new FileOutputStream(filename);
-            
-            ObjectOutput s = new ObjectOutputStream(f);
-            s.writeObject(this);
-            s.close();
-            }
-        //or should be explicitly DBApp exception?
-        catch(Exception e){
-            System.out.println(e.getMessage());
+    // this method return the index of the right page to insert in
+    public int binarySearch(Tuple T) throws Exception
+    {
+    	int key = T.getPK();int mid = 0;
+    	Vector<Page> v =this.getPages();
+	  int l = 0; int r = v.size()-1;
+	  if(r == 0)
+		return 0;
+	                                                                              
+        while (l <= r) {
+             mid = (l + r) / 2;
+ 
+            Page P = Deserialize.Page(v.get(mid).getName());
+            int min = P.getPageProp().get(P.getName()).getMin();
+            int max = P.getPageProp().get(P.getName()).getMax();
+            if (min == key || max == key) 
+                throw new Exception("Duplicate Key");
+             
+ 
+          
+            if(key>min & key<max)
+            	return mid;
+            else
+            	if(max>key)
+            		r = mid - 1;
+            	else
+            		l = mid + 1;
+            	
+            	
         }
-        return filename;
+ 
+     
+        return mid;
     }
+	
+	public static void main(String[]args) throws Exception {
+		
+		
+		
+	}
 
-    /**
-     * @author Brolosy
-     * @param tableFileName the .ser filename of a page
-     * @return the table after deserialisation
-     * @throws Exception either FileNotFoundException or IOException (i think)
-     * Deserialises the specified table.
-     */
-    public Table deserialiseTable(String tableFileName) throws Exception{
-		// Deserialize a string and date from a file.
-        //must make try catch statement when calling this
-		FileInputStream in = new FileInputStream(tableFileName);
-		ObjectInputStream s = new ObjectInputStream(in);
-
-		Table t = (Table)s.readObject();
-		s.close();
-
-        return t;
-    }
-
-
-    
-    
 }
