66328bb2c0bae9e596667a08e4ae7e28e1a66bc7
YousefElbrolosy <yousefelbrolosy@gmail.com>
2024-04-05 14:07:46 +0200

    added missing calls to new

--- src/main/java/DataBaseEngine/DB/DBApp.java
+++ src/main/java/DataBaseEngine/DB/DBApp.java
@@ -191,11 +191,12 @@ public class DBApp {
 
 			SQLTerm[] arrSQLTerms;
 			arrSQLTerms = new SQLTerm[2];
+			arrSQLTerms[0]= new SQLTerm();
 			arrSQLTerms[0]._strTableName =  "Student";
 			arrSQLTerms[0]._strColumnName=  "name";
 			arrSQLTerms[0]._strOperator  =  "=";
 			arrSQLTerms[0]._objValue     =  "John Noor";
-
+			arrSQLTerms[1]= new SQLTerm();
 			arrSQLTerms[1]._strTableName =  "Student";
 			arrSQLTerms[1]._strColumnName=  "gpa";
 			arrSQLTerms[1]._strOperator  =  "=";
