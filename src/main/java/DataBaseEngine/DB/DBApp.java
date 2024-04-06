b0e1a042568458aedce3fc9580c9b5078b96767b
YousefElbrolosy <yousefelbrolosy@gmail.com>
2024-04-06 14:06:20 +0200

    tested stage 1

--- src/main/java/DataBaseEngine/DB/DBApp.java
+++ src/main/java/DataBaseEngine/DB/DBApp.java
@@ -1,6 +1,6 @@
 package DataBaseEngine.DB;
-/** * @author Wael Abouelsaadat */ 
 
+/** * @author Wael Abouelsaadat */
 
 import java.util.Iterator;
 import java.util.Properties;
@@ -19,88 +19,79 @@ import com.opencsv.CSVWriter;
 public class DBApp {
 	final static String csvPath = "metadata.csv";
 
-	public DBApp( ){
+	public DBApp() {
 		this.init();
 	}
 
-	// this does whatever initialization you would like 
-	// or leave it empty if there is no code you want to 
-	// execute at application startup 
+	// this does whatever initialization you would like
+	// or leave it empty if there is no code you want to
+	// execute at application startup
 
-	
-	public void init( ){
-		CSVWriter writer =null;
+	public void init() {
+		CSVWriter writer = null;
 		try {
-			
-			writer = new CSVWriter(new FileWriter(csvPath,true));
-			String [] header = {"Table Name","Column Name","Column Type"
-						,"Clustering Key","IndexName","IndexType"};
-			
+
+			writer = new CSVWriter(new FileWriter(csvPath, true));
+			String[] header = { "Table Name", "Column Name", "Column Type", "Clustering Key", "IndexName",
+					"IndexType" };
+
 			writer.writeNext(header);
-			 writer.flush();
-	            writer.close();
-		}
-		catch(Exception ex) {
+			writer.flush();
+			writer.close();
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		
+
 	}
-	
+
 	// following method creates one table only
 	// strClusteringKeyColumn is the name of the column that will be the primary
 	// key and the clustering column as well. The data type of that column will
 	// be passed in htblColNameType
-	// htblColNameValue will have the column name as key and the data 
+	// htblColNameValue will have the column name as key and the data
 	// type as value
 
+	public void createTable(String strTableName,
+			String strClusteringKeyColumn,
+			Hashtable<String, String> htblColNameType) throws DBAppException, IOException {
 
-	public void createTable(String strTableName, 
-							String strClusteringKeyColumn,  
-							Hashtable<String,String> htblColNameType) throws DBAppException, IOException{
+		Table T = new Table(strTableName, strClusteringKeyColumn, htblColNameType);
+		T.addTable(strTableName, strClusteringKeyColumn, csvPath);
+		Serialize.Table(T);
 
-								
-                 
-								Table T = new Table(strTableName,strClusteringKeyColumn,htblColNameType);
-								T.addTable(strTableName,strClusteringKeyColumn,csvPath);
-								Serialize.Table(T);			
-							
 	}
 
-	// following method creates a B+tree index 
-	public void createIndex(String   strTableName,
-							String   strColName,
-							String   strIndexName) throws DBAppException{
-		
+	// following method creates a B+tree index
+	public void createIndex(String strTableName,
+			String strColName,
+			String strIndexName) throws DBAppException {
+
 		throw new DBAppException("not implemented yet");
 	}
 
-
-
-
-	public static void checkDataType(Hashtable<String,String> htblColNameType) throws DBAppException{
-		for(String s : htblColNameType.values()){
-			if(!(s.equals("java.lang.Integer") || s.equals("java.lang.String") || s.equals("java.lang.Double"))){
+	public static void checkDataType(Hashtable<String, String> htblColNameType) throws DBAppException {
+		for (String s : htblColNameType.values()) {
+			if (!(s.equals("java.lang.Integer") || s.equals("java.lang.String") || s.equals("java.lang.Double"))) {
 				throw new DBAppException("Column DataType invalid");
 			}
 		}
 
 	}
 
-
-	// following method inserts one row only. 
+	// following method inserts one row only.
 	// htblColNameValue must include a value for the primary key
-	public void insertIntoTable(String strTableName, 
-								Hashtable<String,Object>  htblColNameValue) throws DBAppException{
-;
-		
+	public void insertIntoTable(String strTableName,
+			Hashtable<String, Object> htblColNameValue) throws DBAppException {
+		;
+
 		try {
 			Table.checkData(strTableName, htblColNameValue, csvPath);
 		} catch (Exception e) {
-			
+
 			e.printStackTrace();
 		}
 		Table T = Deserialize.Table(strTableName);
-		Tuple record = new Tuple(T.getStrClusteringKeyColumn(),htblColNameValue.keys(),htblColNameValue.elements());	
+		Tuple record = new Tuple(T.getStrClusteringKeyColumn(), htblColNameValue.keys(), htblColNameValue.elements());
 		try {
 			T = T.insertIntoTable(record);
 		} catch (Exception e) {
@@ -108,108 +99,122 @@ public class DBApp {
 			e.printStackTrace();
 		}
 		Serialize.Table(T);
-	
-	}
 
+	}
 
 	// following method updates one row only
-	// htblColNameValue holds the key and new value 
+	// htblColNameValue holds the key and new value
 	// htblColNameValue will not include clustering key as column name
 	// strClusteringKeyValue is the value to look for to find the row to update.
-	public void updateTable(String strTableName, 
-							String strClusteringKeyValue,
-							Hashtable<String,Object> htblColNameValue   )  throws DBAppException{
-	
+	public void updateTable(String strTableName,
+			String strClusteringKeyValue,
+			Hashtable<String, Object> htblColNameValue) throws DBAppException {
+
 		throw new DBAppException("not implemented yet");
 	}
-
 
 	// following method could be used to delete one or more rows.
-	// htblColNameValue holds the key and value. This will be used in search 
-	// to identify which rows/tuples to delete. 	
+	// htblColNameValue holds the key and value. This will be used in search
+	// to identify which rows/tuples to delete.
 	// htblColNameValue enteries are ANDED together
-	public void deleteFromTable(String strTableName, 
-								Hashtable<String,Object> htblColNameValue) throws DBAppException{
-	
+	public void deleteFromTable(String strTableName,
+			Hashtable<String, Object> htblColNameValue) throws DBAppException {
+
 		throw new DBAppException("not implemented yet");
 	}
 
+	public Iterator<Object> selectFromTable(SQLTerm[] arrSQLTerms,
+			String[] strarrOperators) throws DBAppException {
 
-	public Iterator<Object> selectFromTable(SQLTerm[] arrSQLTerms, 
-									String[]  strarrOperators) throws DBAppException{
-		//need to check if table is in MetaData File
-		
+		// need to check if SQLTerm tables are in MetaData File with correct data types
+		// Do I make this Hashtable global so as to decrease spatial complexity by not
+		// creating a hashtable everytime we select?
+		Hashtable<String, Object> htblColNameValue = new Hashtable<String, Object>();
+		try {
+			for (SQLTerm sqlTerm : arrSQLTerms) {
+				// should I say that if table names differ throw exception "joins not
+				// supported?"
+				htblColNameValue.put(sqlTerm._strColumnName, sqlTerm._objValue);
+				Table.checkData(sqlTerm._strTableName, htblColNameValue, csvPath);
+				htblColNameValue.clear();
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 		return null;
 	}
 
-
 	@SuppressWarnings({ "removal" })
-	public static void main( String[] args ){
-	
-		try{
+	public static void main(String[] args) throws Exception {
+
+		try {
 			String strTableName = "Student";
-			DBApp	dbApp = new DBApp( );
-			
-			Hashtable<String,String> htblColNameType = new Hashtable<String,String>( );
+			DBApp dbApp = new DBApp();
+
+			Hashtable<String, String> htblColNameType = new Hashtable<String, String>();
 			htblColNameType.put("id", "java.lang.Integer");
 			htblColNameType.put("name", "java.lang.String");
 			htblColNameType.put("gpa", "java.lang.double");
-			dbApp.createTable( strTableName, "id", htblColNameType );
-			dbApp.createIndex( strTableName, "gpa", "gpaIndex" );
+			dbApp.createTable(strTableName, "id", htblColNameType);
+			dbApp.createIndex(strTableName, "gpa", "gpaIndex");
 
-			Hashtable<String,Object> htblColNameValue = new Hashtable<String,Object>( );
-			htblColNameValue.put("id", new Integer( 2343432 ));
-			htblColNameValue.put("name", new String("Ahmed Noor" ) );
-			htblColNameValue.put("gpa", new Double( 0.95 ) );
-			dbApp.insertIntoTable( strTableName , htblColNameValue );
+			Hashtable<String, Object> htblColNameValue = new Hashtable<String, Object>();
+			htblColNameValue.put("id", new Integer(2343432));
+			htblColNameValue.put("name", new String("Ahmed Noor"));
+			htblColNameValue.put("gpa", new Double(0.95));
+			dbApp.insertIntoTable(strTableName, htblColNameValue);
 
-			htblColNameValue.clear( );
-			htblColNameValue.put("id", new Integer( 453455 ));
-			htblColNameValue.put("name", new String("Ahmed Noor" ) );
-			htblColNameValue.put("gpa", new Double( 0.95 ) );
-			dbApp.insertIntoTable( strTableName , htblColNameValue );
+			htblColNameValue.clear();
+			htblColNameValue.put("id", new Integer(453455));
+			htblColNameValue.put("name", new String("Ahmed Noor"));
+			htblColNameValue.put("gpa", new Double(0.95));
+			dbApp.insertIntoTable(strTableName, htblColNameValue);
 
-			htblColNameValue.clear( );
-			htblColNameValue.put("id", new Integer( 5674567 ));
-			htblColNameValue.put("name", new String("Dalia Noor" ) );
-			htblColNameValue.put("gpa", new Double( 1.25 ) );
-			dbApp.insertIntoTable( strTableName , htblColNameValue );
+			htblColNameValue.clear();
+			htblColNameValue.put("id", new Integer(5674567));
+			htblColNameValue.put("name", new String("Dalia Noor"));
+			htblColNameValue.put("gpa", new Double(1.25));
+			dbApp.insertIntoTable(strTableName, htblColNameValue);
 
-			htblColNameValue.clear( );
-			htblColNameValue.put("id", new Integer( 23498 ));
-			htblColNameValue.put("name", new String("John Noor" ) );
-			htblColNameValue.put("gpa", new Double( 1.5 ) );
-			dbApp.insertIntoTable( strTableName , htblColNameValue );
+			htblColNameValue.clear();
+			htblColNameValue.put("id", new Integer(23498));
+			htblColNameValue.put("name", new String("John Noor"));
+			htblColNameValue.put("gpa", new Double(1.5));
+			dbApp.insertIntoTable(strTableName, htblColNameValue);
 
-			htblColNameValue.clear( );
-			htblColNameValue.put("id", new Integer( 78452 ));
-			htblColNameValue.put("name", new String("Zaky Noor" ) );
-			htblColNameValue.put("gpa", new Double( 0.88 ) );
-			dbApp.insertIntoTable( strTableName , htblColNameValue );
-
+			htblColNameValue.clear();
+			htblColNameValue.put("id", new Integer(78452));
+			htblColNameValue.put("name", new String("Zaky Noor"));
+			htblColNameValue.put("gpa", new Double(0.88));
+			dbApp.insertIntoTable(strTableName, htblColNameValue);
 
 			SQLTerm[] arrSQLTerms;
 			arrSQLTerms = new SQLTerm[2];
-			arrSQLTerms[0]= new SQLTerm();
-			arrSQLTerms[0]._strTableName =  "Student";
-			arrSQLTerms[0]._strColumnName=  "name";
-			arrSQLTerms[0]._strOperator  =  "=";
-			arrSQLTerms[0]._objValue     =  "John Noor";
-			arrSQLTerms[1]= new SQLTerm();
-			arrSQLTerms[1]._strTableName =  "Student";
-			arrSQLTerms[1]._strColumnName=  "gpa";
-			arrSQLTerms[1]._strOperator  =  "=";
-			arrSQLTerms[1]._objValue     =  new Double( 1.5 );
+			arrSQLTerms[0] = new SQLTerm();
+			arrSQLTerms[0]._strTableName = "Student";
+			arrSQLTerms[0]._strColumnName = "name";
+			arrSQLTerms[0]._strOperator = "=";
+			arrSQLTerms[0]._objValue = "John Noor";
+			arrSQLTerms[1] = new SQLTerm();
+			arrSQLTerms[1]._strTableName = "Student";
+			arrSQLTerms[1]._strColumnName = "gpa";
+			arrSQLTerms[1]._strOperator = "=";
+			arrSQLTerms[1]._objValue = new Double(1.5);
 
-			String[]strarrOperators = new String[1];
+			String[] strarrOperators = new String[1];
 			strarrOperators[0] = "OR";
 			// select * from Student where Student.name = "John Noor" or Student.gpa = 1.5;
-			Iterator<Object> resultSet = dbApp.selectFromTable(arrSQLTerms , strarrOperators);
+			// will they all be from the same table? [ie all sql terms will have the same
+			// table]
+			// I think yes because no joins
+			Iterator<Object> resultSet = dbApp.selectFromTable(arrSQLTerms, strarrOperators);
+			
+			System.out.println("brolos");
+		} catch (Exception exp) {
+			exp.printStackTrace();
 		}
-		catch(Exception exp){
-			exp.printStackTrace( );
-		}}
-	}
 		
-	
+	}
+}
--- src/main/java/DataBaseEngine/DB/Test.java
+++ src/main/java/DataBaseEngine/DB/Test.java
@@ -13,108 +13,128 @@ import java.util.Properties;
 import java.util.Vector;
 
 public class Test {
-	//private 
-	//Properties prop;
-	public Test()  throws Exception{
-		
+	// private
+	// Properties prop;
+	public Test() throws Exception {
+
 	}
+
 	public int getMaxCount() throws IOException {
 		String filename = "DBApp.config";
 		Properties prop = null;
-		try(FileInputStream fis = new FileInputStream(filename) ){
+		try (FileInputStream fis = new FileInputStream(filename)) {
 			prop = new Properties();
 			prop.load(fis);
-			
-		}catch(FileNotFoundException ex) {
+
+		} catch (FileNotFoundException ex) {
 			ex.printStackTrace();
 		}
-	  String num = prop.getProperty("MaximumRowsCountinPage");
-	  return Integer.parseInt(num);
-		
+		String num = prop.getProperty("MaximumRowsCountinPage");
+		return Integer.parseInt(num);
+
 	}
 
 	@SuppressWarnings({ "removal", "deprecation" })
 	public static void main(String[] args) throws Exception {
-		
+
 		Test T = new Test();
 		int max = T.getMaxCount();
-		System.out.println(max); //200
-		/* 
-		Page p = new Page("Student");
-		Page p1 = new Page("Student");
-		Page p2 = new Page("Book");
-		System.out.println(p.getName());
-		System.out.println(p1.getName());
-		System.out.println(p2.getName());
-		*/
-	/*	try{
-			
-			Page p = new Page("PC");
-			// Serialize created table to a file and appending to array of tables.
-			FileOutputStream f = new FileOutputStream("PC.ser");
+		System.out.println(max); // 200
+		/*
+		 * Page p = new Page("Student");
+		 * Page p1 = new Page("Student");
+		 * Page p2 = new Page("Book");
+		 * System.out.println(p.getName());
+		 * System.out.println(p1.getName());
+		 * System.out.println(p2.getName());
+		 */
+		/*
+		 * try{
+		 * 
+		 * Page p = new Page("PC");
+		 * // Serialize created table to a file and appending to array of tables.
+		 * FileOutputStream f = new FileOutputStream("PC.ser");
+		 * 
+		 * ObjectOutput s = new ObjectOutputStream(f);
+		 * s.writeObject(p);
+		 * s.close();
+		 * }
+		 * //or should be explicitly DBApp exception?
+		 * catch(Exception e){
+		 * System.out.println(e.getMessage());
+		 * }
+		 * 
+		 * // Deserialize a string and date from a file.
+		 * FileInputStream in = new FileInputStream("PC.ser");
+		 * ObjectInputStream s = new ObjectInputStream(in);
+		 * 
+		 * Page p = (Page)s.readObject();
+		 * s.close();
+		 * System.out.println(p.getName());
+		 * 
+		 * 
+		 * 
+		 * 
+		 * Hashtable<String,Object> attributesInTuple = new Hashtable<String,Object>();
+		 * attributesInTuple.put("id", new Integer( 23498 ));
+		 * attributesInTuple.put("name", new String("John Noor" ) );
+		 * attributesInTuple.put("gpa", new Double( 1.5 ) );
+		 * 
+		 * Tuple t1 = new Tuple(attributesInTuple);
+		 * 
+		 * 
+		 * Hashtable<String,Object> attributesInTuple2 = new Hashtable<String,Object>();
+		 * attributesInTuple2.put("id", new Integer( 2615 ));
+		 * attributesInTuple2.put("name", new String("brolo" ) );
+		 * attributesInTuple2.put("gpa", new Double( 1.2 ) );
+		 * 
+		 * Tuple t2 = new Tuple(attributesInTuple2);
+		 * 
+		 * String res = "";
+		 * Enumeration<Object> en = attributesInTuple.elements();
+		 * 
+		 * while (en.hasMoreElements()) {
+		 * Object val = en.nextElement();
+		 * 
+		 * res = val + res;
+		 * if(en.hasMoreElements()){
+		 * res = "," + res;
+		 * }
+		 * 
+		 * }
+		 * System.out.println(res);
+		 * //testing the page toString()
+		 * Vector<Tuple> tuplesInPage = new Vector<Tuple>();
+		 * tuplesInPage.add(t1);
+		 * tuplesInPage.add(t2);
+		 * String res2 = "";
+		 * for(int i = 0; i<tuplesInPage.size(); i++){
+		 * res2 = res2 + tuplesInPage.elementAt(i).toString();
+		 * if(i!=tuplesInPage.size()-1){
+		 * res2 = res2 + ",";
+		 * }
+		 * }
+		 * 
+		 * //makes all separated by commas
+		 * System.out.println(res2);
+		 * 
+		 */
+		DBApp d = new DBApp();
+		SQLTerm[] arrSQLTerms;
+		arrSQLTerms = new SQLTerm[2];
+		arrSQLTerms[0] = new SQLTerm();
+		arrSQLTerms[0]._strTableName = "Student";
+		arrSQLTerms[0]._strColumnName = "name";
+		arrSQLTerms[0]._strOperator = "=";
+		arrSQLTerms[0]._objValue = "John Noor";
+		arrSQLTerms[1] = new SQLTerm();
+		arrSQLTerms[1]._strTableName = "Student";
+		arrSQLTerms[1]._strColumnName = "gpa";
+		arrSQLTerms[1]._strOperator = "=";
+		arrSQLTerms[1]._objValue = new Double(1.5);
 
-			ObjectOutput s = new ObjectOutputStream(f);
-			s.writeObject(p);
-			s.close();
-			}
-			//or should be explicitly DBApp exception?
-			catch(Exception e){
-				System.out.println(e.getMessage());
-			}
-
-			// Deserialize a string and date from a file.
-		FileInputStream in = new FileInputStream("PC.ser");
-		ObjectInputStream s = new ObjectInputStream(in);
-
-		Page p = (Page)s.readObject();
-		s.close();
-		System.out.println(p.getName());
-
-
-
-	
-	Hashtable<String,Object> attributesInTuple = new Hashtable<String,Object>();
-	attributesInTuple.put("id", new Integer( 23498 ));
-	attributesInTuple.put("name", new String("John Noor" ) );
-	attributesInTuple.put("gpa", new Double( 1.5 ) );
-	
-	Tuple t1 = new Tuple(attributesInTuple);
-
-
-	Hashtable<String,Object> attributesInTuple2 = new Hashtable<String,Object>();
-	attributesInTuple2.put("id", new Integer( 2615 ));
-	attributesInTuple2.put("name", new String("brolo" ) );
-	attributesInTuple2.put("gpa", new Double( 1.2 ) );
-
-	Tuple t2 = new Tuple(attributesInTuple2);
-
-	String res = "";
-	Enumeration<Object> en = attributesInTuple.elements();
-
-	while (en.hasMoreElements()) {
-		Object val = en.nextElement();
-
-		res = val + res;
-		if(en.hasMoreElements()){
-			res = "," + res;
-		}
-	
+		String[] strarrOperators = new String[1];
+		strarrOperators[0] = "OR";
+		d.selectFromTable(arrSQLTerms,strarrOperators);
 	}
-	System.out.println(res);
-	//testing the page toString()
-	Vector<Tuple> tuplesInPage = new Vector<Tuple>();
-	tuplesInPage.add(t1);
-	tuplesInPage.add(t2);
-	String res2 = "";
-	for(int i = 0; i<tuplesInPage.size(); i++){
-		res2 = res2 + tuplesInPage.elementAt(i).toString();
-		if(i!=tuplesInPage.size()-1){
-			res2 = res2 + ",";
-		}
-	}
-
-	//makes all separated by commas
-	System.out.println(res2);
-
-*/
-	}}
+}
